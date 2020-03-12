d67ced4508a0f1cfd5e8c011d90bccf3e4903164
Vihanga nivarthana <31271508+VihangaN@users.noreply.github.com>
2020-03-12 14:35:00 +0530

    update

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import home from './components/home.vue';
+import summery from './components/summery.vue'
+
+Vue.use(Router)
+
+let router =  new Router({
+    routes:[
+       {path:'/' , component:home,props: true},
+        {path:'/summery' , component:summery},
+      
+    ],
+    mode:'history',
+    linkExactActiveClass: 'active',
+    
+  });
+
+  export default router;
