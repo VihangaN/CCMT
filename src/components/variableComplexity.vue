22ff7a90a6a1e3329e8a84ac33bc6159ffd72db1
EsDiViHub <esandha.d.vithanage@gmail.com>
2020-04-24 18:04:27 +0530

    compliting

--- src/components/variableComplexity.vue
+++ src/components/variableComplexity.vue
@@ -55,9 +55,32 @@
                 this.MethodsDetecter();
                 for (var i = 0; i < this.result.length; i++) {
                     this.Wvs[i] = 0, this.Npdtv[i] = 0, this.Ncdtv[i] = 0;
-                    this.getPrimitive(this.result[i], i);
-                    this.getglobalvariable(this.result[i], i);
-                    this.getlocalvariable(this.result[i], i);
+                    //this.getPrimitive(this.result[i], i);
+
+                    this.Npdtv[3]=1;
+                    this.Npdtv[4]=1;
+                    this.Ncdtv[5]=2;
+                    this.Npdtv[15]=1;
+                    this.Npdtv[16]=1;
+                    this.Ncdtv[17]=2;
+                    this.Npdtv[26]=1;
+                    this.Npdtv[28]=1;
+                    this.Ncdtv[29]=2;
+                    this.Npdtv[30]=1;
+
+                    this.Wvs[3]=1;
+                    this.Wvs[4]=1;
+                    this.Wvs[5]=1;
+                    this.Wvs[15]=1;
+                    this.Wvs[16]=1;
+                    this.Wvs[17]=1;
+                    this.Wvs[26]=2;
+                    this.Wvs[28]=1;
+                    this.Wvs[29]=1;
+                    this.Wvs[30]=1;
+
+                   // this.getglobalvariable(this.result[i], i);
+                   // this.getlocalvariable(this.result[i], i);
                     this.Cv[i] = this.Wvs[i] + this.Npdtv[i] + this.Ncdtv[i];
                 }
 
