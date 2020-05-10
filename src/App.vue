a6a3ce6ddda88ce7f0b41531bd50efd06c603287
vihanga nivarthana <vihanganivarthana@vihangas-MacBook-Air.local>
2020-05-10 08:44:31 +0530

    added vue-x to manage state and updated some ui logics for validations

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,104 @@
 <template>
-    <v-app id="inspire">
-        <v-navigation-drawer
-                v-model="drawer"
-                :clipped="$vuetify.breakpoint.lgAndUp"
-                app
-        >
-            <v-list dense nav>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app v-if="getnav">
+      <v-list dense nav link >
+        <!-- navigation list items -->
 
+        <v-list-item id="upload">
+          <router-link to="/">
+            <v-list-item-content id="lic">
+              <!-- <v-list-item-title>Upload</v-list-item-title> -->
+              <v-btn class="ma-2 white--text"
+              color="#000"
+              id="button-upload"
+              @click="upload"
+              >
+                Upload
+                <v-icon right dark>mdi-cloud-upload</v-icon>
+              </v-btn>
+            </v-list-item-content>
+          </router-link>
+        </v-list-item>
+<v-list-item-group v-show="getsub">
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-equal-box</v-icon>
+          </v-list-item-icon>
+          <router-link to="/totalComplex">
+            <v-list-item-content>
+              <v-list-item-title>Total complex</v-list-item-title>
+            </v-list-item-content>
+          </router-link>
+        </v-list-item>
 
-                <!-- navigation list items -->
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-move-resize-variant</v-icon>
+          </v-list-item-icon>
+          <router-link to="/summery">
+            <v-list-item-content>
+              <v-list-item-title>size complex</v-list-item-title>
+            </v-list-item-content>
+          </router-link>
+        </v-list-item>
 
-                <v-list-item link>
-                    <v-list-item-icon>
-                        <v-icon>mdi-upload</v-icon>
-                    </v-list-item-icon>
-                    <router-link to="/">
-                    <v-list-item-content>
-                        <v-list-item-title>Upload</v-list-item-title>
-                    </v-list-item-content>
-                    </router-link>
-                </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-tooltip-text</v-icon>
+          </v-list-item-icon>
+          <router-link to="/variableComplexity">
+            <v-list-item-content>
+              <v-list-item-title>Variable Complexity</v-list-item-title>
+            </v-list-item-content>
+          </router-link>
+        </v-list-item>
 
-                <v-list-item link>
-                    <v-list-item-icon>
-                        <v-icon>mdi-upload</v-icon>
-                    </v-list-item-icon>
-                    <router-link to="/totalComplex">
-                    <v-list-item-content>
-                        <v-list-item-title>Total complex</v-list-item-title>
-                    </v-list-item-content>
-                    </router-link>
-                </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-code-braces</v-icon>
+          </v-list-item-icon>
+          <router-link to="/methodComplexity">
+            <v-list-item-content>
+              <v-list-item-title>Method Complexity</v-list-item-title>
+            </v-list-item-content>
+          </router-link>
+        </v-list-item>
 
-                <v-list-item link>
-                    <v-list-item-icon>
-                        <v-icon>mdi-upload</v-icon>
-                    </v-list-item-icon>
-                    <router-link to="/summery">
-                        <v-list-item-content>
-                            <v-list-item-title>size complex</v-list-item-title>
-                        </v-list-item-content>
-                    </router-link>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-pipe-disconnected</v-icon>
+          </v-list-item-icon>
+          <router-link to="/cuplingComplex">
+            <v-list-item-content>
+              <v-list-item-title>cupling Complexity</v-list-item-title>
+            </v-list-item-content>
+          </router-link>
+        </v-list-item>
 
-                </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-google-controller</v-icon>
+          </v-list-item-icon>
+          <router-link to="/contolStructerComplex">
+            <v-list-item-content>
+              <v-list-item-title>contolStructer Complexity</v-list-item-title>
+            </v-list-item-content>
+          </router-link>
+        </v-list-item>
 
-                <v-list-item link>
-                    <v-list-item-icon>
-                        <v-icon>mdi-upload</v-icon>
-                    </v-list-item-icon>
-                    <router-link to="/variableComplexity">
-                        <v-list-item-content>
-                            <v-list-item-title>Variable Complexity</v-list-item-title>
-                        </v-list-item-content>
-                    </router-link>
-
-                </v-list-item>
-
-                <v-list-item link>
-                <v-list-item-icon>
-                    <v-icon>mdi-upload</v-icon>
-                </v-list-item-icon>
-                <router-link to="/methodComplexity">
-                    <v-list-item-content>
-                        <v-list-item-title>Method Complexity</v-list-item-title>
-                    </v-list-item-content>
-                </router-link>
-            </v-list-item>
-
-                <v-list-item link>
-                    <v-list-item-icon>
-                        <v-icon>mdi-upload</v-icon>
-                    </v-list-item-icon>
-                    <router-link to="/cuplingComplex">
-                        <v-list-item-content>
-                            <v-list-item-title>cupling Complexity</v-list-item-title>
-                        </v-list-item-content>
-                    </router-link>
-                </v-list-item>
-
-                <v-list-item link>
-                    <v-list-item-icon>
-                        <v-icon>mdi-upload</v-icon>
-                    </v-list-item-icon>
-                    <router-link to="/contolStructerComplex">
-                        <v-list-item-content>
-                            <v-list-item-title>contolStructer Complexity</v-list-item-title>
-                        </v-list-item-content>
-                    </router-link>
-                </v-list-item>
-
-                <v-list-item link>
-                    <v-list-item-icon>
-                        <v-icon>mdi-upload</v-icon>
-                    </v-list-item-icon>
-                    <router-link to="/inheritanceComplex">
-                        <v-list-item-content>
-                            <v-list-item-title>inheritance Complexity</v-list-item-title>
-                        </v-list-item-content>
-                    </router-link>
-                </v-list-item>
-
-            </v-list>
-        </v-navigation-drawer>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-file-tree</v-icon>
+          </v-list-item-icon>
+          <router-link to="/inheritanceComplex">
+            <v-list-item-content>
+              <v-list-item-title>inheritance Complexity</v-list-item-title>
+            </v-list-item-content>
+          </router-link>
+        </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark id="nav">
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
@@ -111,7 +108,7 @@
 
       <v-spacer />
 
-      <v-btn icon large disabled="">
+      <v-btn icon large disabled>
         <v-img src="./assets/logo.svg" alt="Logo" class="logo"></v-img>
       </v-btn>
     </v-app-bar>
@@ -119,24 +116,65 @@
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
           <transition name="fade" mode="out-in">
+     
             <router-view></router-view>
           </transition>
         </v-row>
       </v-container>
     </v-content>
-    <v-btn bottom color="" dark fab fixed right @click="upload" id="fab">
+    <v-btn bottom color dark fab fixed right @click="upload" id="fab">
       <v-icon>mdi-plus</v-icon>
     </v-btn>
+
+    
   </v-app>
 </template>
 
 <script>
 import router from "./router";
+import {mapState , mapGetters , mapMutations} from 'vuex'
 export default {
   props: {
     source: String
   },
+  computed:{
+    ...mapGetters([
+      'getnav',
+      'getsub'
+
+    ]),
+    ...mapState([
+      'nav',
+      'sub'
+    ]),
+},
+ created(){
+  
+      if (performance.navigation.type == 1) {
+        if(this.$route.path != '/') {
+          if (confirm('Reload site?. Change you made may not be saved.')) {
+               this.setSub(false)
+               this.setType(false)
+                 this.$router.push({path: '/'})
+           
+          } else {
+            console.log('reload page now');
+            
+         
+                this.setSub(true)
+               this.setType(true)
+           
+               
+             
+        }
+      }
+  }
+ },
+
+ 
   data: () => ({
+    
+    confirm:false,
     dialog: false,
     drawer: null,
     items: [
@@ -169,11 +207,29 @@ export default {
       { icon: "mdi-keyboard", text: "Go to the old version" }
     ]
   }),
+
+  
   methods: {
     upload() {
       router.push("/");
-    }
-  }
+      this.setType(false)
+    },
+     ...mapMutations([
+      'setnav',
+      'setsub'
+    ]),
+    setType: function(status) {
+      this.setnav(status)
+  
+    },
+
+    setSub: function(status) {
+     this.setsub(status)
+  
+    },
+    
+  },
+ 
 };
 </script>
 
@@ -200,12 +256,40 @@ export default {
   right: 30px;
 }
 
-#fab{
-background-image: linear-gradient(180deg, #23C5DA 0%, #5772A5 100%) !important;
+#fab {
+  background-image: linear-gradient(
+    180deg,
+    #23c5da 0%,
+    #5772a5 100%
+  ) !important;
 }
 
-#nav{
-    background: #258ad3 !important;
-    /* box-shadow: none !important; */
+#nav {
+  background: #258ad3 !important;
+  /* box-shadow: none !important; */
+}
+#upload {
+  text-align: center !important;
+  display: flex !important;
+  align-items: center !important;
+  justify-content: center;
+  width: 100% !important;
+  margin: 20px 0px 20px;
+ 
+}
+
+#lic{
+  width: 100% !important;
+  margin-right: 50px !important;
+  padding-right:20px ;
+}
+
+#button-upload{
+  width:50px !important;
+  background-image: linear-gradient(
+    10deg,
+    #23c5da 0%,
+    #5772a5 100%
+  ) !important;
 }
 </style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,103 +1,189 @@
-<template>
-    <div>
+<template >
+  <div id="print">
+    <!-- <input type="file" ref="myFile" @change="selectedFile"><br/> -->
 
-        <!-- <input type="file" ref="myFile" @change="selectedFile"><br/> -->
+    <b-field class="centered">
+      <b-upload
+        v-model="files"
+        multiple
+        accept=".java"
+        type="is-info"
+        id="up"
+        drag-drop
+        ref="document"
+      >
+        <section class="section">
+          <div class="content has-text-centered">
+            <p>
+              <b-icon icon="upload" size="is-large"></b-icon>
+            </p>
+            <p>Drop your java files here or click to upload</p>
+          </div>
+        </section>
+      </b-upload>
 
+      <label>{{message}}</label>
+    </b-field>
 
-        <b-field class="centered">
-            <b-upload v-model="files"
-                multiple
-                drag-drop>
-                <section class="section">
-                    <div class="content has-text-centered">
-                        <p>
-                            <b-icon
-                                icon="upload"
-                                size="is-large">
-                            </b-icon>
-                        </p>
-                        <p>Drop your files here or click to upload</p>
-                    </div>
-                </section>
-            </b-upload>
-
-             <label>{{message}}</label>
-        </b-field>
-
-         <div class="tags" id="tags">
-            <span v-for="(file, index) in files"
-                :key="index"
-                class="tag is-primary" >
-                {{file.name}}
-                <button class="delete is-small"
-                    type="button"
-                    @click="deletefile(index)">
-                </button>
-            </span>
-        </div>
-
-        <div id="scan" v-if="files.length > 0">
-            <b-button type="is-danger"
-                icon-left="delete">
-                Start Now
-            </b-button>
-        </div>
+    <div class="tags" id="tags">
+      <span v-for="(file, index) in files" :key="index" class="tag is-primary">
+        {{file.name}}
+        <button class="delete is-small" type="button" @click="deletefile(index)"></button>
+      </span>
     </div>
+
+    <div id="scan" v-if="files.length > 0">
+      <b-button type="is-info" icon-left="autorenew" @click="selectedFile">Start Now</b-button>
+    </div>
+
+   
+
+    <button @click="exportToPDF"> {{test}}</button>
+  </div>
 </template>
 
 <script>
-    export default {
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
+import {mapState , mapGetters , mapMutations} from 'vuex'
 
-        data() {
-            return {
-                text: '',
-                message: '',
-                textWithoutComment: '',
-                files: [] //  store multiple files
-            }
+
+export default {
+
+computed:{
+    ...mapGetters([
+      'getnav',
+      'getsub'
+
+    ]),
+    ...mapState([
+      'nav',
+      'sub'
+    ]),
+},
+
+  data() {
+    return {
+      text: "",
+      message: "",
+      textWithoutComment: "",
+      files: [] //  store multiple files
+    };
+  },
+  watch:{
+      files: function (n) {
+         if(n.length > 0){
+        this.setType(true);
+         }
+      }
+  },
+  
+  methods: {
+      ...mapMutations([
+      'setnav',
+      'setsub'
+    ]),
+    setType: function(status) {
+      this.setnav(status)
+  
+    },
+    setSub: function(status) {
+      this.setsub(status)
+  
+    },
+
+    selectedFile() {
+        this.setType(true);
+      console.log("File uploaded");
+      this.checkSuccess();
+      let file = this.$refs.myFile.files[0];
+      let reader = new FileReader();
+      reader.readAsText(file, "UTF-8");
+      reader.onload = evt => {
+        this.text = evt.target.result;
+        localStorage.setItem("filedata", this.text);
+        this.message = "File upload successful";
+      };
+      reader.onerror = evt => {
+        this.message = console.error(evt);
+      };
+       
+    },
+    deletefile(file) {
+      this.files.splice(file, 1); // splice the files array by passed index value
+    },
+    checkSuccess() {
+         
+      if (this.files.length > 0) {
+           this.setSub(true);
+        this.$buefy.toast.open({
+          duration: 1000,
+          message: "Files uploaded successfully !",
+          position: "is-bottom-right",
+          type: "is-success"
+        });
+      
+      } else {
+        this.$buefy.toast.open({
+          duration: 1000,
+          message: "Files not uploaded successfully",
+          position: "is-bottom-right",
+          type: "is-danger"
+        });
+      }
+    },
+
+    exportToPDF() {
+      window.html2canvas = html2canvas;
+
+      var doc = new jsPDF("p", "pt", "a4");
+
+    
+      var source = document.getElementById("print").innerHTML;
+
+      var margins = {
+        top: 10,
+        bottom: 10,
+        left: 10,
+        width: 595
+      };
+
+      doc.fromHTML(
+        source, 
+        margins.left,
+        margins.top,
+        {
+          width: margins.width
         },
-        methods: {
-            selectedFile() {
-                console.log('File uploaded');
-                let file = this.$refs.myFile.files[0];
-                let reader = new FileReader();
-                reader.readAsText(file, "UTF-8");
-                reader.onload = evt => {
-                    this.text = evt.target.result;
-                    localStorage.setItem("filedata", this.text);
-                    this.message = "File upload successful"
-                }
-                reader.onerror = evt => {
-                    this.message =
-                        console.error(evt);
 
-                }
-            },
-             deletefile(file) {
-                this.files.splice(file, 1); // splice the files array by passed index value
-            }
-        }
-
+        function() {
+          
+          doc.save("Test.pdf");
+        },
+        margins
+      );
     }
+  }
+};
 </script>
 
 <style scoped>
-.centered{
-    position: absolute;
-    left:50%;
-    top:50%;
-    transform: translate(-50%,-50%);
+.centered {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
 
-#tags{
-    margin-top:300px;
-    display: block;
+#tags {
+  margin-top: 300px;
+  display: block;
 }
 
-#scan{
-    position: absolute;
-    left:50%;
-    top:80%;
-    transform: translate(-50%,-50%);
+#scan {
+  position: absolute;
+  left: 50%;
+  top: 80%;
+  transform: translate(-50%, -50%);
 }
 </style>
