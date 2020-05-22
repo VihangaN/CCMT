aa8f193512d6af69817526740c00a9d6bc5a5052
EsDiViHub <esandha.d.vithanaga@gmail.com>
2020-05-23 05:13:19 +0530

    implement print method

--- src/components/cuplingComplex.vue
+++ src/components/cuplingComplex.vue
@@ -14,58 +14,64 @@
         <div v-for="res in cordoutput.length" :key="res">
 
             <div :id="filename[res-1]" v-if="show == filename[res-1]">
-                <h3>{{filename[res-1]}}</h3>
+                <h3>{{filename[res-1]}}
+                    <b-button type="is-info" @click="print('print'+filename[res-1])">Print</b-button>
+
+                </h3>
                 <br>
-                <table>
-            <thead>
-            <tr>
-                <th class="text-left">#</th>
-                <th class="text-left"></th>
-                <th class="text-left">Nr</th>
-                <th class="text-left">Nmcms</th>
-                <th class="text-left">Nmcmd</th>
-                <th class="text-left">Nmcrms</th>
-                <th class="text-left">Nmcrmd</th>
-                <th class="text-left">Nrmcrms</th>
-                <th class="text-left">Nrmcrmd</th>
-                <th class="text-left">Nrmcms</th>
-                <th class="text-left">Nrmcmd</th>
-                <th class="text-left">Nmrgvs</th>
-                <th class="text-left">Nmrgvd</th>
-                <th class="text-left">Nrmrgvs</th>
-                <th class="text-left">Nrmrgvd</th>
-                <th class="text-left">Ccp</th>
+                <table :id="'print'+filename[res-1]" class="printBg">
+                    <thead>
+                    <tr>
+                        <th class="text-left">#</th>
+                        <th class="text-left"></th>
+                        <th class="text-left">Nr</th>
+                        <th class="text-left">Nmcms</th>
+                        <th class="text-left">Nmcmd</th>
+                        <th class="text-left">Nmcrms</th>
+                        <th class="text-left">Nmcrmd</th>
+                        <th class="text-left">Nrmcrms</th>
+                        <th class="text-left">Nrmcrmd</th>
+                        <th class="text-left">Nrmcms</th>
+                        <th class="text-left">Nrmcmd</th>
+                        <th class="text-left">Nmrgvs</th>
+                        <th class="text-left">Nmrgvd</th>
+                        <th class="text-left">Nrmrgvs</th>
+                        <th class="text-left">Nrmrgvd</th>
+                        <th class="text-left">Ccp</th>
 
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="line in cordoutput[res-1].length" :key="line">
-                <td>{{line-1}}</td>
-                <td class="code">{{cordoutput[res-1][line-1]}}</td>
-                <td>{{Ccp[res-1][line-1][0]}}</td>
-                <td>{{Ccp[res-1][line-1][1]}}</td>
-                <td>{{Ccp[res-1][line-1][2]}}</td>
-                <td>{{Ccp[res-1][line-1][3]}}</td>
-                <td>{{Ccp[res-1][line-1][4]}}</td>
-                <td>{{Ccp[res-1][line-1][5]}}</td>
-                <td>{{Ccp[res-1][line-1][6]}}</td>
-                <td>{{Ccp[res-1][line-1][7]}}</td>
-                <td>{{Ccp[res-1][line-1][8]}}</td>
-                <td>{{Ccp[res-1][line-1][9]}}</td>
-                <td>{{Ccp[res-1][line-1][10]}}</td>
-                <td>{{Ccp[res-1][line-1][11]}}</td>
-                <td>{{Ccp[res-1][line-1][12]}}</td>
-                <td>{{Ccp[res-1][line-1][13]}}</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="line in cordoutput[res-1].length" :key="line">
+                        <td>{{line-1}}</td>
+                        <td class="code">{{cordoutput[res-1][line-1]}}</td>
+                        <td>{{Ccp[res-1][line-1][0]}}</td>
+                        <td>{{Ccp[res-1][line-1][1]}}</td>
+                        <td>{{Ccp[res-1][line-1][2]}}</td>
+                        <td>{{Ccp[res-1][line-1][3]}}</td>
+                        <td>{{Ccp[res-1][line-1][4]}}</td>
+                        <td>{{Ccp[res-1][line-1][5]}}</td>
+                        <td>{{Ccp[res-1][line-1][6]}}</td>
+                        <td>{{Ccp[res-1][line-1][7]}}</td>
+                        <td>{{Ccp[res-1][line-1][8]}}</td>
+                        <td>{{Ccp[res-1][line-1][9]}}</td>
+                        <td>{{Ccp[res-1][line-1][10]}}</td>
+                        <td>{{Ccp[res-1][line-1][11]}}</td>
+                        <td>{{Ccp[res-1][line-1][12]}}</td>
+                        <td>{{Ccp[res-1][line-1][13]}}</td>
 
-            </tr>
-            </tbody>
-        </table>
+                    </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import htmlToImage from "html-to-image";
+    import download from "downloadjs";
+
     export default {
         name: "cuplingComplex",
         components: {},
@@ -77,8 +83,7 @@
             cordoutput: [],
             filename: [],
             show: "",
-            Ccp:[]
-
+            Ccp: []
 
 
         }),
@@ -97,7 +102,12 @@
             }
         },
         methods: {
-
+            print(id) {
+                htmlToImage.toPng(document.getElementById(id))
+                    .then(function (dataUrl) {
+                        download(dataUrl, new Date().toLocaleString() + " " + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
+                    });
+            }
         }
     }
 </script>
@@ -131,6 +141,14 @@
 
     tr:nth-child(odd) {
         background-color: #eee;
+    }
+
+    tr:nth-child(even) {
+        background-color: #fff;
+    }
+
+    .printBg {
+        background-color: #fff;
     }
 
     #menubtn {
